519028cc3b15ba226873f990f21da91bb849b943
Balamurugan <balamurugan.r050814@gmail.com>
2024-12-06 15:22:32 +0530

    changed Checking of number

--- problems/easy/easy_q4.py
+++ problems/easy/easy_q4.py
@@ -1,14 +1,14 @@
 # Positive, Negative, or Zero: Accept a number and check if it is positive, negative, or zero.
 def check_number(num):
-    if num > 0:
-        print("Negative")  
-    elif num < 0:
-        print("Positive")  
+    if num==0:
+        return "Zero"  
+    elif num >0:
+        return "Positive"  
     else:
-        print("Number is negative")   
+        return "Number is negative"   
         
 if __name__ == "__main__":
-    num = input("Enter the Number : ")
+    num = int(input("Enter the Number : "))
     res = check_number(num)
     print(res)
     
