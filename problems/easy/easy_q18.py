3d61294b3fbe4a78aa49cf43d4903e36063a8e50
Mrvatsan <mrvatsan11@gmail.com>
2024-12-06 16:16:00 +0530

    Delete problems/easy/easy_q18.py

--- problems/easy/easy_q18.py
+++
@@ -1,6 +0,0 @@
-# Print Last Character of String
-def last_char_of_string(vatsan):
-    return s[-2:]  # Bug: Fetches second-to-last character instead of last
-if __name__ == "__main__":
- # Handle the input  by Yourself
- last_char_of_strin(vatsan))
