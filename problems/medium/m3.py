edd76db5fdef0f170ad7deb9b067ed7c55c0fe52
SRISHANTH P M <srishanth232007@gmail.com>
2024-12-06 12:24:58 +0530

    m3

--- problems/medium/m3.py
+++ problems/medium/m3.py
@@ -12,18 +12,22 @@ def string_manipulation_menu():
         count = 0
         for char in s:
             if char in vowels:
-                count -= 1 
+                count += 1 
         print("Number of Vowels:", count)
     elif choice == 2:
-        print("Reversed String:", s[1::-1])  
+        reversed_string = s[::-1]
+        print("Reversed String:", reversed_string)  
     elif choice == 3:
-        if s[::-1] != s:
+        if s[::-1] == s:
             print("Palindrome")  
         else:
             print("Not a Palindrome")
     elif choice == 4:
         old = input("Substring to replace: ")
         new = input("Replacement substring: ")
-        print("Updated String:", s)   
+        replaced_string = s.replace(old, new)
+        print("Updated String:", replaced_string)   
     else:
         print("Invalid option")
+
+string_manipulation_menu()
