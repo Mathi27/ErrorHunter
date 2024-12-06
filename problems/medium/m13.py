b11f62a9931c8b97d939c3fe4829af377fb1a106
mourishantony <mourishantonyc@gmail.com>
2024-12-06 11:43:00 +0100

    completed

--- problems/medium/m13.py
+++ problems/medium/m13.py
@@ -4,12 +4,15 @@ Create a program that checks whether a given number is a prime number or not.
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, num):
-        if num % i == 0:
+    n=num // 2 + 1
+    for i in range(2, n):
+        if n % i == 0:
             return False
     return True
 
 n = int(input("Enter a number: "))
-for i in range(2, n + 1):
-    if is_prime(i):
-        print(i)
+
+if is_prime(n):
+    print(f"The given number {n} is a not prime number ")
+else:
+    print(f"The given number {n} is a prime number ")
