547e07600723a28f55fad39ff2e7da6eed239204
Mirdula18 <mirdula1801@gmail.com>
2024-12-06 13:46:08 +0530

    debugged_q5

--- problems/easy/easy_q5.py
+++ problems/easy/easy_q5.py
@@ -1,17 +1,19 @@
 # Grading System: Write a program that takes a student’s marks as input and prints the grade (A, B, C, or F) based on given thresholds.
 
 def grade_system(marks):
-    if marks >= 90:
-        return "B"  
-    elif marks >= 80:
-        return "A"   
-    elif marks >= 70:
-        return "F"   
-    else:
+    if marks < 100 and marks >= 90:
+        return "A"  
+    elif marks < 90 and marks >= 80:
+        return "B"   
+    elif marks <80 and marks >= 70:
         return "C"  
+    elif marks < 0 or marks > 100:
+        return "Invalid marks" 
+    else:
+        return "F"  
     
 if __name__ == "__main__":
-      num = input("Enter the Mark : ")
+      num = int(input("Enter the Mark : "))
       res = grade_system(num)
       print(res)
       
