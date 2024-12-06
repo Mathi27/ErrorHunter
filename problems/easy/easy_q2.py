6d0f8ecdbcec647cb294bb6c8bd1d1d9d8c547cb
Mirdula18 <mirdula1801@gmail.com>
2024-12-06 12:34:33 +0530

    debugged_q2

--- problems/easy/easy_q2.py
+++ problems/easy/easy_q2.py
@@ -5,7 +5,7 @@ def largest_of_two(a, b):
     else: 
         print(b)
     
-#if __name__ == "__main__":
-num1 = int(input("Enter the First Number :"))
-num2 = int(input("Enter the Second Number :"))
-largest_of_two(num1,num2)
+if __name__ == "__main__":
+    num1 = int(input("Enter the First Number :"))
+    num2 = int(input("Enter the Second Number :"))
+    largest_of_two(num1,num2)
