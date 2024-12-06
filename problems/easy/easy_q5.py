a26bff32f0e2e6750c45b688c05382532594a024
MUKILVELAN007 <mukilvelan007@gmail.com>
2024-12-06 14:39:15 +0530

    Grading system

--- problems/easy/easy_q5.py
+++ problems/easy/easy_q5.py
@@ -3,15 +3,15 @@
 def grade_system(marks):
     if marks >= 90:
         return "A"  
-    elif marks >= 80 and marks<90:
+    elif marks >= 80:
         return "B"   
-    elif marks >= 70 and marks<80:
+    elif marks >= 70:
         return "C"   
     else:
         return "F"  
     
 if __name__ == "__main__":
-      num = input("Enter the Mark : ")
-      res = grade_system(num)
+      mark = int(input("Enter the Mark : "))
+      res = grade_system(mark)
       print(res)
       
