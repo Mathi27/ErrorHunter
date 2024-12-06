a52ad73dd8ad7d4483e9f1491b1a90b7a5cdcde6
Mirdula18 <mirdula1801@gmail.com>
2024-12-06 16:03:53 +0530

    debugged_m3

--- problems/medium/m3.py
+++ problems/medium/m3.py
@@ -1,29 +1,38 @@
-def string_manipulation_menu():
-    print("1. Count Vowels")
-    print("2. Reverse String")
-    print("3. Check Palindrome")
-    print("4. Replace Substring")
-    choice = int(input("Enter your choice: "))
+def string_manipulation_menu(choice):
 
     s = input("Enter a string: ")
 
     if choice == 1:
-        vowels = "aeiouAEIOU"
+        vowels = 'a','e','i','o','u','A','E','I','O','U'
         count = 0
         for char in s:
             if char in vowels:
-                count -= 1 
+                count += 1 
         print("Number of Vowels:", count)
     elif choice == 2:
-        print("Reversed String:", s[1::-1])  
+        print("Reversed String:", s[::-1])  
     elif choice == 3:
-        if s[::-1] != s:
+        if s[::-1] == s:
             print("Palindrome")  
         else:
             print("Not a Palindrome")
     elif choice == 4:
         old = input("Substring to replace: ")
         new = input("Replacement substring: ")
+        string = ""
+        for char in s:
+            if char == old:
+                new = char
+                string += char
+            else:
+                string += char    
         print("Updated String:", s)   
     else:
         print("Invalid option")
+
+print("1. Count Vowels")
+print("2. Reverse String")
+print("3. Check Palindrome")
+print("4. Replace Substring")
+choice = int(input("Enter your choice: "))
+string_manipulation_menu(choice)
