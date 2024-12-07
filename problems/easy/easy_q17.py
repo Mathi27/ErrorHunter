2c32436daed0ffe3377eafbcd81d7e80353eda57
Mirdula18 <mirdula1801@gmail.com>
2024-12-07 07:25:14 +0530

    debugged_q17

--- problems/easy/easy_q17.py
+++ problems/easy/easy_q17.py
@@ -1,7 +1,9 @@
 # Print X N Times
 def print_x_n_times(x, n):
-    for i in range(1, n):  # Bug: Loop runs one less time than expected
+    for i in range(1, n + 1):  # Bug: Loop runs one less time than expected
         print(x)
 if __name__ == "__main__":
  # Handle the input  by Yourself
- print_x_n_times()
+ num = int(input("Enter number: "))
+ rep = int(input("Enter number of times to repeat: "))
+ print_x_n_times(num,rep)
