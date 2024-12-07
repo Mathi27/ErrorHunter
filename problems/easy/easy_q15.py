fe6f39c8ebe35ceb85507ff73d28d15465b999c1
Mirdula18 <mirdula1801@gmail.com>
2024-12-07 07:14:09 +0530

    debugged_q15

--- problems/easy/easy_q15.py
+++ problems/easy/easy_q15.py
@@ -1,14 +1,18 @@
 # Grade Description: Write a program that accepts a grade (A, B, C, D, F) and prints its description (e.g., A = Excellent, B = Good, etc.) using a switch case.
 def grade_description(grade):
     switch = {
-        'A': "Good",
-        'B': "Average",
-        'C': "Poor",
-        'D': "Excellent",  
+        'A': "Excellent",
+        'B': "Good",
+        'C': "Average",
+        'D': "Poor",  
         'F': "Fail"
     }
-    return switch.get(grade, "Not a valid grade") 
+    if grade in switch:
+        return switch.get(grade, "Not a valid grade") 
+    else: 
+        return "Invalid Grade"
 if __name__ == "__main__":
-    rs = grade_description('Z')
+    ngrade = input("Enter grade: ")
+    rs = grade_description(ngrade)
     print(rs)
     
