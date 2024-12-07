fafaf78767a9a5041f344d2a55c06edb0bc2d05c
Mirdula18 <mirdula1801@gmail.com>
2024-12-07 06:55:35 +0530

    debugged_q10

--- problems/easy/easy_q10.py
+++ problems/easy/easy_q10.py
@@ -1,12 +1,15 @@
 # Check Palindrome Number: Use a do-while loop to determine if a given number is a palindrome
 def is_palindrome(num):
-    original = num
+    temp = num
     reverse = 0
-    do:
-        reverse = reverse * 10 + num % 10
-        num //= 10
-    while num != 0 
-    return reverse == original
+    while temp > 0:
+        reverse = reverse * 10 + temp % 10
+        temp //= 10
+    if num == reverse:
+        return "Palindrome"
+    else:
+        return  "Not a Palindrome"
+        #return reverse == original
 if __name__ == "__main__":
     nums = int(input("Enter the Number: "))
     res = is_palindrome(nums)
