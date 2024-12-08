214357cc44cb948158c226cbdb168f595e20ff50
BoomikaSN <boomikasn2006@gmail.com>
2024-12-08 06:48:24 +0530

    debugged the errors in check if a number is an armstrong

--- problems/medium/m15.py
+++ problems/medium/m15.py
@@ -1,29 +1,21 @@
 '''Write a program to check if a number is an Armstrong number (e.g., 153  = 1^3 + 5^3 + 3^3 ) .'''
 def count_digits(n):
-   i = 0
-   while n >= 0:
-      n //= 10
-      i += 1
-   return i
+    i = 0
+    while n > 0:  
+        n //= 10  
+        i += 1    
+    return i
 
-def sum(n):
-   i = count_digits(n)
-   s = 0
-   while n > 0:
-      digit = n/10
-      n /= 10
-      s += pow(digit,i)
-   return s
+def calculate_sum(n):
+    original = n  
+    i = count_digits(n)
+    s = 0
+    while n > 0:
+        digit = n % 10  
+        s += pow(digit, i)  
+        n //= 10  
+    return s
 
 
-num = 1634
-
- 
-s = sum(num)
-
- 
-if s == num:
-   print('Given number is an Armstrong Number')
-else:
-   print('Given number is not an Armstrong Number')
-
+number = int(input("Enter a number: "))
+print("Sum of digits raised to the power of count:", calculate_sum(number))
