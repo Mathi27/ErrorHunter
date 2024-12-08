e34026f708cfc86f0021c41379c3386d2b67d679
Dharshini-457 <dharshinir619@gmail.com>
2024-12-08 11:15:24 +0530

    sum of the array elements commit

--- problems/easy/easy_q16.py
+++ problems/easy/easy_q16.py
@@ -3,13 +3,13 @@ def sum_first_last(arr):
     return arr[0] + arr[-1]  
 if __name__ == "__main__":
     # Handle the input  by Yourself
- 
-    arr=[]
+    arr=[2,5,8,7,6]
+    
     n=int(input("Enter the Total number of Elements:"))
     for i in range(n):
         x=int(input("Enter the element :"))
         arr.append(x)
  
-    arr=[2,5,8,7,6]
- 
+   
+    print(arr)
     print(sum_first_last(arr))
