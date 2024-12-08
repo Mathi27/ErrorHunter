b8cd8465a13d8272e9fdcff56ac57ad78c1e2a9d
Srihari-tech2dive <sriharicbe07@gmail.com>
2024-12-08 10:23:49 +0530

    I have changed the code in m1

--- problems/medium/m1.py
+++ problems/medium/m1.py
@@ -1,37 +1,36 @@
-'''
-Create a menu to perform basic mathematical operations (addition, subtraction, multiplication, division, modulo) on two numbers.
-
-'''
+choice=int(input("Enter no. of choice:"))
 def math_operations_menu(choice):
-    a, b = map(int, input("Enter two numbers(separated by commas): ").split(sep=","))
-
-
-    if choice == 2:
-        print("Subtraction:", a - b)   
-    elif choice == 1:
-        print("Addition:", a + b)   
-    elif choice == 4:
-        print("Division:", a / b)   
-    elif choice == 3:
-        print("Multiplication:", a * b)   
- 
+    a, b = map(int,input("Enter two numbers(separated by commas): ").split(sep=",")) 
     if choice == 1:
-        print(f"Subtraction of {a} and {b}:{a - b}")   
+        print(a + b)   
     elif choice == 2:
-        print(f"Addition of {a} and {b}:{a + b}")   
+         print(a - b)   
+    elif choice == 3:
+        print(a * b)   
+    elif choice == 4:
+         print(a / b)   
+    elif choice == 5:
+        print(a % b)   
+    elif choice == 6:
+       print(a ** b)   
+
+    if choice == 1:
+        print(f"Addition of {a} and {b}:{a + b}")      
+    elif choice == 2:
+       print(f"Subtraction of {a} and {b}:{a - b}")
     elif choice == 3:
         print(f"Division of {a} and {b}:{a / b}")   
     elif choice == 4:
         print(f"Multiplication of {a} and {b}:{a * b}")   
-     elif choice == 5:
-        print(f"Modulus of {a} and {b}:{a // b}")   
+    elif choice == 4:
+        print(f"Division of {a} and {b}:{a / b}")
+    elif choice == 5:
+        print(f"Modulus of {a} and {b}:{a % b}")   
     elif choice == 6:
         print(f"{a} to the power of {b}:{a**b}")
     else:
         print("Invalid option")
-math_operations_menu()
- 
-        print("Invalid option!!!")
+math_operations_menu(choice)
 print("-------------Mathematical operation menu---------------")
 print("1. Add")
 print("2. Subtract")
@@ -39,7 +38,7 @@ print("3. Multiply")
 print("4. Divide")
 print("5. Modulus")
 print("6. Expontential")
-choice = int(input("Enter your choice: "))
-math_operations_menu(choice)
+
+
 print("-------------------------------------------------------")
  
