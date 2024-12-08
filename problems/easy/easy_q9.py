4027719f2e146a20e47750d7ddbfe5dad3ce016a
Dharshini-457 <dharshinir619@gmail.com>
2024-12-08 10:15:43 +0530

    Factorial

--- problems/easy/easy_q9.py
+++ problems/easy/easy_q9.py
@@ -1,18 +1,15 @@
 # Factorial of a Number: Write a program to calculate the factorial of a given number using a while loop.
 def factorial(num):
     result = 1
-    while n > 0:
+    for n in range(1,num+1):
         result *= n
         n += 1   
     return result
 
 if __name__ == "__main__":
  
-    n= int(input("Enter the Number :"))
-    print(factorial(n))
+    num= int(input("Enter the Number :"))
+    print(f"Factorial of the number {num} is ",factorial(num))
 
  
-    num = int(input("Enter the Number :"))
-    factorial(num*7)
-    print(num)
     
