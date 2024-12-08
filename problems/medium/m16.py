d19edb2b2f8a53c61ae3a408e712dab5a3de9e10
BoomikaSN <boomikasn2006@gmail.com>
2024-12-08 06:43:54 +0530

    debugged the errors in generate random password

--- problems/medium/m16.py
+++ problems/medium/m16.py
@@ -3,8 +3,11 @@ import random
 import string
 
 def generate_password(length):
-    characters = string.ascii_letters + string.digits
-    return "".join(random.choice(characters) for i in range(length))
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return "".join(random.choice(characters) for _ in range(length))
 
 length = int(input("Enter password length: "))
-print(generate_password(length))
+if length < 4:
+    print("Password length must be at least 4 characters.")
+else:
+    print("Generated Password:", generate_password(length))
