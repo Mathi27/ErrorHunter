dabb806e7b20beb031522a1ff796037e4afbc2e2
BoomikaSN <boomikasn2006@gmail.com>
2024-12-08 06:54:32 +0530

    debugged the errors in given number is prime or not

--- problems/medium/m13.py
+++ problems/medium/m13.py
@@ -4,12 +4,15 @@ Create a program that checks whether a given number is a prime number or not.
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, num):
+
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
     return True
 
+
 n = int(input("Enter a number: "))
-for i in range(2, n + 1):
-    if is_prime(i):
-        print(i)
+if is_prime(n):
+    print(f"{n} is a prime number.")
+else:
+    print(f"{n} is not a prime number.")
