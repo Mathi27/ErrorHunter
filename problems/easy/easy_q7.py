b336b58ead9d3f1dab35890d84e310298fe6523f
muzhumathi30 <muzhumathi30@gmail.com>
2025-07-31 11:07:27 +0530

    q7

--- problems/easy/easy_q7.py
+++ problems/easy/easy_q7.py
@@ -1,53 +1,14 @@
 def sum_of_digits(num):
     """Calculate the sum of digits of a number."""
     total = 0
-    num = abs(num)   
+    num = abs(num)  # Handle negative numbers
+
     while num > 0:
- 
-        total += (num % 10) 
-        num //=10
-    return total
-
-if __name__ == "__main__":
-    num = int(input("Enter the Number : "))
-    total=sum_of_digits(num)
-    print("Sum of digits:",total)
-
- 
-        
- 
- 
-        digits = num%10
-        total += digits 
- 
-        num //= 10
-    return total
-
-if __name__ == "__main__":
-    number = int(input("Enter the Number : "))
- 
-    print(sum_of_digits(number))
- 
-    print("Sum of digits:",sum_of_digits(number))
-    
-    
-    
- 
- 
-        n=num%10
-        total+=n
-        num=num//10
- 
- 
         total += num % 10
- 
- 
-        num //= 10  
- 
- 
+        num //= 10
+
     return total
 
- 
-        num //= 10          
-    return total
- 
+if __name__ == "__main__":
+    num = int(input("Enter the Number: "))
+    print("Sum of digits:", sum_of_digits(num))
