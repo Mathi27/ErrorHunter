1475a8dbdfb9a2a0b9716183ac637eb51c5dc112
Logeshwari N <logeshwarinagaraj30@gmail.com>
2025-07-31 11:26:10 +0530

    easy_q9

--- problems/easy/easy_q9.py
+++ problems/easy/easy_q9.py
@@ -1,38 +1,11 @@
 # Factorial of a Number: Write a program to calculate the factorial of a given number using a while loop.
-def factorial(n):
-    if n < 0:
-        raise ValueError("Factorial is not defined for negative numbers.")
-    result = 1
- 
-    i=1
-    while i <= n:
-        result *= i
-        i += 1   
- 
-    while n > 0:
- 
-        result*=n
-        n -= 1   
- 
-        result *= n
-        n -= 1   
- 
-    return result
- 
-if __name__ == "__main__":
- 
-    n= int(input("Enter the Number :"))
-    print(factorial(n))
-
- 
-    num = int(input("Enter the Number :"))
-  
- 
-    print(factorial(num))
- 
- 
-     res=factorial(num)
-    print(res)
-     
- 
- 
+n= int(input("Enter the Number :"))
+fact=1
+i=1
+if n < 0:
+    print("Factorial number caanot be negative!")
+else:
+        while i<=n:
+              fact*=i
+              i+=1 
+        print(f"Factorial of {n} is {fact}")
