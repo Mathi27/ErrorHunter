8cde93c6027d642438db4a86c5f1f422944e2933
Logeshwari N <logeshwarinagaraj30@gmail.com>
2025-07-31 10:40:09 +0530

    easy_q3

--- problems/easy/easy_q3.py
+++ problems/easy/easy_q3.py
@@ -1,20 +1,10 @@
 # Leap Year or Not: Write a program to determine whether a given year is a leap year.
-def is_leap_year(year):
- 
+try:
+    year = int(input("Enter the number :"))
     if year % 4 == 0:
         print("leap year")
     else:
         print("not a leap year")
 
-if __name__ == "__main__":
-    
-    num = int(input("Enter the number :"))
-    is_leap_year(num)
- 
-    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:   
- 
-        return "Leap Year"
-    else:   
- 
-        return "Not a Leap Year"
- 
+except ValueError:
+    print("Enter only integer number!!")
