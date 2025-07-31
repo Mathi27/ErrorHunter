87739e4af00c46fca47c64bfb591e71ab3dba6c4
Sowdeswari26 <sowdeswari47@gmail.com>
2025-07-31 10:26:57 +0530

    Added if-else condition

--- FunProjects/Grades.py
+++ FunProjects/Grades.py
@@ -3,9 +3,15 @@ class StudentGrades:
         self.grades = {}
 
     def add_grade(self, student, grade):
-        self.grades[student] = grade
-        print(student,"Grade is ",self.grades[student])
+        if isinstance(grade, (int, float)):
+            self.grades[student] = grade
+            print(student, "Grade is", self.grades[student])
+        else:
+            print(f"Invalid grade for {student}. Grade must be a number.")
+
     def calculate_average(self):
+        if not self.grades:
+            return 0  # Avoid division by zero
         total = sum(self.grades.values())
         average = total / len(self.grades)
         return average
@@ -15,4 +21,4 @@ grades = StudentGrades()
 grades.add_grade("John", 85)
 grades.add_grade("Sarah", 90)
 grades.add_grade("Mike", 78)
-print("Average grade:", grades.calculate_average())  
+print("Average grade:", grades.calculate_average())
