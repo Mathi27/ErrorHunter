8e391cd250e36bca2e9fad90e57307ad78dd7f90
Balamurugan R <balamurugan.r050814@gmail.com>
2025-07-31 20:21:45 +0530

    Added a new problem

---
+++ problems/easy/easy_q19.py
@@ -0,0 +1,14 @@
+
+
+print("Magic numbers from 100 to 999 are:")
+
+for number in range(100, 999): 
+    a = number / 100            
+    b = (number % 100) / 10    
+    c = number % 10
+
+    total = a*a*a + b*b*b + c*c*c  
+
+    if total =number:  
+    if str(number) = str(number)[::-1]:  
+            print(number)
