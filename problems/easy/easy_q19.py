8251bc221728b61511fe8dd145895a355239b8c3
Balamurugan R <balamurugan.r050814@gmail.com>
2025-07-31 20:25:36 +0530

    Added a new problem easy 19

--- problems/easy/easy_q19.py
+++ problems/easy/easy_q19.py
@@ -1,5 +1,10 @@
+"""
+A magic number is a number which satisfy the below condition:
+1.The number must be an Armstrong Number
+2.It must a palindrome Number 
 
-
+and the number lies between 100 and 999
+"""
 print("Magic numbers from 100 to 999 are:")
 
 for number in range(100, 999): 
