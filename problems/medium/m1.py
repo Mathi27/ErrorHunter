eee7b8022dc50a5e7a90af7bc5c73b3d21c9dfd1
Logeshwari N <logeshwarinagaraj30@gmail.com>
2025-07-31 12:02:16 +0530

    m1

--- problems/medium/m1.py
+++ problems/medium/m1.py
@@ -5,102 +5,39 @@ Create a menu to perform basic mathematical operations (addition, subtraction, m
 '''
  
  
-def math_operations_menu(a,b,operator):
-   
- 
- 
-def math_operations_menu():
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-    print("5. Modulo")
- 
-    while True:
-        try:
-            choice = int(input("Enter your choice: "))
-            a = int(input("Enter number 1:"))
-            b = int(input("Enter number 2:"))
-            break
-        except:
-            print("Enter a number!")
+print("-------------Mathematical operation menu--------'1. Add' , '2. subtract' , '3. multiply' , '4.divide' , '5.modulus' , '6.expontential'")
+choice = int(input("Enter your choice: "))
+a = int(input("Enter number 1:"))
+b = int(input("Enter number 2:"))
 
-    while True:
-        if choice == 1:
-            print("Addition:", a + b)
-            break 
-        elif choice == 2:
-            print("Subtraction:", a - b)
-            break     
-        elif choice == 3:
-            print("Multiplication:", a * b)
-            break
-        elif choice == 4:
-            print("Division:", a / b)
-            break     
-        elif choice == 5:
-            print("Modulo:", a % b)
-            break   
-        else:
-            print("Invalid option")
-math_operations_menu()
- 
- 
-    choice = int(input("Enter your choice: "))
- 
-
-    a, b = map(int, input("Enter two numbers: ").split(sep=","))
-
- 
-    if choice == 1:
-        print("Addition:", a + b)   
-    elif choice == 2:
-        print("Subtraction:", a - b)   
- 
- 
- 
- 
-    a=int(input("enter a number1:"))
-    b=int(input("enter a number2:"))
- 
- 
-    if choice == 2:
-        print("Subtraction:", a - b)   
-    elif choice == 1:
-        print("Addition:", a + b)   
-    elif choice == 4:
- 
-        print("Division:", a / b)   
- 
-        print("Division:", a / b)   
- 
-        if b==0:
+if choice == 1:
+        add = a+b
+        print("Addition:", add)
+elif choice == 2:
+        sub = a-b
+        print("Subtraction:", sub)    
+elif choice == 3:
+        mul = a*b
+        print("Multiplication:", mul)
+elif choice == 4:
+        if b!=0:
+            div = a/b
+            print("Division:", div)
+        elif b==0:
             print("zero error")
         else:
-            print("Division:", a / b)   
- 
- 
- 
-    elif choice == 3:
-        print("Multiplication:", a * b)   
-    elif choice == 4:
-        print("Divide:", a / b)   
-    elif choice == 5:
- 
-        print(f"Modulus of {a} and {b}:{a // b}")   
-    elif choice == 6:
-        print(f"{a} to the power of {b}:{a**b}")
-    else:
-        print("Invalid option!!!")
-print("-------------Mathematical operation menu--------------------")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-print("5. Modulus")
-print("6. Expontential")
- 
- )
- 
+            print("invalid")   
+elif choice == 5:
+        mod = a%b
+        print("Modulo:", mod)
+elif choice == 6:
+        expo=a**b
+        print("Expontential:", expo)  
+else:
+        print("Invalid option")
+
+
+
+
  
  
