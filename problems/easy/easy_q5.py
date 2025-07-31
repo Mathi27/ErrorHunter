f638160a4de1ac2e7234ebefcf3ee6f6995b93f9
marzuk1910 <marzukahamed1910@gmail.com>
2025-07-31 11:29:40 +0530

    Updated eq3

--- problems/easy/easy_q5.py
+++ problems/easy/easy_q5.py
@@ -1,22 +1,11 @@
-def grade_system(marks):
- 
-    if marks >= 90:
-        return "A"  
-    elif marks >= 80:
-        return "B"   
-    elif marks >= 70:
- 
-        return "c"   
-    else:
-        return "F"  
-    
-if __name__ == "__main__":
-      num = int(input("Enter the Mark : "))
-      res = grade_system(num)
-      print(res)
- 
-        return "C"   
-    else:
-        return "F"  
+def grade_system(m):
+    if m >= 90: return "A"
+    elif m >= 80: return "B"
+    elif m >= 70: return "C"
+    else: return "F"
+
+marks = int(input("Enter Marks: "))
+print(grade_system(marks))
+  
 
  
