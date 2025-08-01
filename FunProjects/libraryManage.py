e2ddd5d541f1521e0ca03d5fb8dc0b87726ec2af
Logeshwari N <logeshwarinagaraj30@gmail.com>
2025-08-01 11:42:29 +0530

    library manage

--- FunProjects/libraryManage.py
+++ FunProjects/libraryManage.py
@@ -4,35 +4,32 @@ class Library:
 
     def add_book(self, title, author):
         self.books[title] = {"author": author, "available": True}
-        print(f"Book {title} added.")
+        print(f"Book '{title}' added.")
 
     def borrow_book(self, title):
         if title not in self.books:
-            print(f"{title} not available.")
-        elif self.books[title]["available"] == False:
+            print(f"'{title}' not found in library.")
+        elif not self.books[title]["available"]:
             print(f"'{title}' is already borrowed.")
         else:
-            self.books[title]["available"] = True
+            self.books[title]["available"] = False
             print(f"You borrowed '{title}'.")
 
     def return_book(self, title):
- 
-        if self.books[title]["available"] == False:
- 
-        if  title not in self.books:
-           print(f"'{title}' is not in the library.")
-        elif self.books[title]["available"] == True:
- 
+        if title not in self.books:
+            print(f"'{title}' is not in the library.")
+        elif self.books[title]["available"]:
             print(f"'{title}' was not borrowed.")
         else:
             self.books[title]["available"] = True
-            print(f"You returned '{title}'")
+            print(f"You returned '{title}'.")
+
     def display_books(self):
         for title, details in self.books.items():
             status = "Available" if details["available"] else "Not Available"
             print(f"Title: {title}, Author: {details['author']}, Status: {status}")
 
- 
+# Test the system
 library = Library()
 library.add_book("1984", "George Orwell")
 library.borrow_book("1984")
